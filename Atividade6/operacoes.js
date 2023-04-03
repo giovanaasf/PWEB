b6de3f0543de6b234fb37d77d2f066162f99473c
Giovana Freitas <99348623+giovanaasf@users.noreply.github.com>
2023-04-03 12:38:37 -0300

    Add files via upload

---
+++ Atividade6/operacoes.js
@@ -0,0 +1,13 @@
+
+        var num1;
+        var num2;
+
+        num1 = parseFloat (prompt("Digite o numero 1: "));
+        num2 = parseFloat (prompt("Digite o numero 2: "));
+
+        alert("Soma: " + num1 + " + "+ num2 + " = " + (num1 + num2));
+        alert("Subtração: " + num1 + " - "+ num2 + " = " + (num1 - num2).toFixed(2));
+        alert("Produto: " + num1 + " * "+ num2 + " = " + (num1 * num2).toFixed(2));
+        alert("Divisão: " + num1 + " / "+ num2 + " = " + (num1 / num2).toFixed(2));
+        alert("Resto: " + num1 + " % "+ num2 + " = " + (num1 % num2).toFixed(2));
+
